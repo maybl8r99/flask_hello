c9f3dc71d972cfb0a1fac8906f879c7e0779dfe2
maybl8r99 <reuben.wee@gmail.com>
2015-08-20 13:08:33 +0800

    Use render_template

--- default.py
+++ default.py
@@ -1,7 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import builtins
+builtins.unicode = str
+
+from flask.ext.triangle import Triangle
 import socket
 
 app = Flask (__name__)
+Triangle(app)
 
 hostname = socket.gethostname()
 debug_hostnames = ['EastWind-Server.local',]
@@ -10,7 +15,7 @@ debug_hostnames = ['EastWind-Server.local',]
 def default():
     if (request.args.get('debug') == '0'):
         app.debug = False
-    return 'Hello world.\n' + hostname + ' debug mode is ' + str(app.debug)
+    return render_template('default.html', is_debug='abc')
 
 if __name__ == '__main__':
     app.debug = hostname in debug_hostnames
