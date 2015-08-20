d7700adb4966070b8aa5ea58ae825678a009d95a
maybl8r99 <reuben.wee@gmail.com>
2015-08-20 13:10:57 +0800

    added comment

--- default.py
+++ default.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
+# discovered Py3 incompatibility with Flask Triangle, unicode issue
 import builtins
 builtins.unicode = str
+# end unicode assignment to allow Flask Triangle to work with Python 3
 
 from flask.ext.triangle import Triangle
 import socket
